922a2c8eb612504947cbb883398b193e0ce8152c
Chris (from laptop/git desktop app) <38088640+chuynh18@users.noreply.github.com>
2018-07-26 20:04:34 -0700

    curried sum now has unlimited arguments version

--- curried.js
+++ curried.js
@@ -6,8 +6,7 @@
 const sum = function(firstNum) {
     if (arguments.length > 1) {
         return arguments[0] + arguments[1];
-    }
-    else {
+    } else {
         return function(secondNum) {
             return firstNum+secondNum;
         }
@@ -16,6 +15,7 @@ const sum = function(firstNum) {
 
 console.log(sum(2, 3)); // 5
 console.log(sum(2)(3)); // 5
+
 
 // what about...
 // sum(2, 3, 4); // 9
@@ -28,8 +28,7 @@ const sum3 = function(firstNum) {
             sum += arguments[i];
         }
         return sum;
-    }
-    else {
+    } else {
         return function(secondNum) {
             return function(thirdNum) {
                 return firstNum+secondNum+thirdNum;
@@ -40,3 +39,32 @@ const sum3 = function(firstNum) {
 
 console.log(sum3(2, 3, 4)); // 9
 console.log(sum3(2)(3)(4)); // 9
+
+
+// what about summing an arbitrarily long number of operands?
+// sumUnlimited(1, 2, 3, 4, 5, 6, 7, 8, 9, 10); // 55
+// sumUnlimited(1)(2)(3)(4)(5)(6)(7)(8)(9)(10); // 55
+
+// well, it's not exactly like the above...
+const sumUnlimited = function(arg) {
+    if (arguments.length > 1) {
+        let sum = 0;
+        for (let i = 0; i < arguments.length; i++) {
+            sum += arguments[i];
+        }
+        return sum;
+    } else {
+        let count = arg;
+        return function curried(next) {
+            if (typeof next !== "number") {
+                return count;
+            } else {
+                count += next;
+                return curried;
+            }
+        }
+    }
+}
+
+console.log(sumUnlimited(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)); // 55
+console.log(sumUnlimited(1)(2)(3)(4)(5)(6)(7)(8)(9)(10)("STOP!  HAMMERTIME")); // 55 - need to end with a non-number
